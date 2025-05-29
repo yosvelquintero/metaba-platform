cbda768acd3f44e4d3b96cf5b6bb3b0ceba5bdf3
Yosvel Quintero <yosvelquintero@gmail.com>
2025-05-29 12:23:18 +0700

    chore: update Next.js configuration to enable SVGR and Babel upward root mode

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -9,7 +8,10 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  nx: {
+    svgr: true,
+    babelUpwardRootMode: true,
+  },
 };
 
 const plugins = [
